901c0b234dd26ccedb8ebfd01e3d0013857afc0d
nepp-plus <nepp.plus@gmail.com>
2022-06-18 14:47:01 +0900

    들고 돌아온 프래그먼트에서, 닉네임 반영 처리  - Activity.RESULT_OK 만 제외하고는 액티비티와 코딩 방법 동일

--- app/src/main/java/com/neppplus/orderapp_20220618/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/orderapp_20220618/fragments/MyProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.neppplus.orderapp_20220618.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -29,6 +30,23 @@ class MyProfileFragment : Fragment() {
 
             val myIntent = Intent( requireContext(), EditNicknameActivity::class.java )
             startActivityForResult(myIntent, REQ_CODE_FOR_NICKNAME)
+
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQ_CODE_FOR_NICKNAME) {
+
+            if (resultCode == Activity.RESULT_OK) {
+
+                val nickname = data!!.getStringExtra("nickname")
+
+                txtNickname.text = nickname
+
+            }
 
         }
 
