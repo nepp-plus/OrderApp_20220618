d0f33af555dd5f0d6ca03ac8c661ac92a244e70b
nepp-plus <nepp.plus@gmail.com>
2022-06-18 14:41:52 +0900

    닉네임 변경 저장 > 입력한 닉네임 들고 복귀

--- app/src/main/java/com/neppplus/orderapp_20220618/EditNicknameActivity.kt
+++ app/src/main/java/com/neppplus/orderapp_20220618/EditNicknameActivity.kt
@@ -1,7 +1,9 @@
 package com.neppplus.orderapp_20220618
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,19 @@ class EditNicknameActivity : AppCompatActivity() {
     }
 
     fun setupEvents() {
+
+        btnSave.setOnClickListener {
+
+            val inputNickname = edtNickname.text.toString()
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("nickname", inputNickname )
+
+            setResult(RESULT_OK, resultIntent)
+
+            finish()
+
+        }
 
     }
 
