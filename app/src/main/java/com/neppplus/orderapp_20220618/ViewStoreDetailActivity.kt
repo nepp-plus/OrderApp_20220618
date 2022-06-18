b523188ef4698ea09809098308fdff7200054b83
nepp-plus <nepp.plus@gmail.com>
2022-06-18 14:15:07 +0900

    전화 주문 버튼 이벤트 처리  - TedPermission 라이브러리  - ACTION_CALL 기능 활용

--- app/src/main/java/com/neppplus/orderapp_20220618/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/neppplus/orderapp_20220618/ViewStoreDetailActivity.kt
@@ -1,8 +1,14 @@
 package com.neppplus.orderapp_20220618
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import com.neppplus.orderapp_20220618.datas.StoreData
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
@@ -19,6 +25,36 @@ class ViewStoreDetailActivity : AppCompatActivity() {
     }
 
     fun setupEvents() {
+
+        btnCall.setOnClickListener {
+
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+
+                    val myUri = Uri.parse("tel:${mStoreData.phoneNum}")
+                    val myIntent = Intent( Intent.ACTION_CALL, myUri )
+                    startActivity(myIntent)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                    Toast.makeText(
+                        this@ViewStoreDetailActivity,
+                        "통화 권한을 허가해야 전화 주문이 가능합니다.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+
+            }
+
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions( Manifest.permission.CALL_PHONE )
+                .check()
+
+        }
 
     }
 
