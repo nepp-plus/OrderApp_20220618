8d0c160fac85b0b3f06d853039e534479e2f15d3
nepp-plus <nepp.plus@gmail.com>
2022-06-18 11:33:01 +0900

    리스트뷰 어댑터 - getView 함수 뼈대 완성

--- app/src/main/java/com/neppplus/orderapp_20220618/adapters/StoreAdapter.kt
+++ app/src/main/java/com/neppplus/orderapp_20220618/adapters/StoreAdapter.kt
@@ -1,7 +1,11 @@
 package com.neppplus.orderapp_20220618.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.orderapp_20220618.R
 import com.neppplus.orderapp_20220618.datas.StoreData
 
 class StoreAdapter(
@@ -9,4 +13,19 @@ class StoreAdapter(
     resId: Int,
     val mList: ArrayList<StoreData>
 ) : ArrayAdapter<StoreData>( mContext, resId, mList ) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if (tempRow == null) {
+
+            tempRow = LayoutInflater.from( mContext ).inflate( R.layout.store_list_item, null )
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+
 }
