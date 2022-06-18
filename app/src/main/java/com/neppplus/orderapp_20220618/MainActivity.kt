794d8aae4b53b352415e703e4f08ea26348bacea
nepp-plus <nepp.plus@gmail.com>
2022-06-18 10:41:22 +0900

    뷰페이저 / 어댑터 연결 + 탭레이아웃 / 뷰페이저 연결  - 페이지별 제목 설정

--- app/src/main/java/com/neppplus/orderapp_20220618/MainActivity.kt
+++ app/src/main/java/com/neppplus/orderapp_20220618/MainActivity.kt
@@ -2,10 +2,31 @@ package com.neppplus.orderapp_20220618
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.orderapp_20220618.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mvpa : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues() {
+
+        mvpa = MainViewPagerAdapter( supportFragmentManager )
+        mainViewPager.adapter = mvpa
+
+        mainTabLayout.setupWithViewPager( mainViewPager )
+
+    }
+
 }
