35381616703b099dd18a80b0fb5cd771a843774e
nepp-plus <nepp.plus@gmail.com>
2022-06-18 11:43:40 +0900

    어댑터 / 리스트뷰 연결 (프래그먼트 내부에서)

--- app/src/main/java/com/neppplus/orderapp_20220618/fragments/PizzaStoreFragment.kt
+++ app/src/main/java/com/neppplus/orderapp_20220618/fragments/PizzaStoreFragment.kt
@@ -6,11 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.orderapp_20220618.R
+import com.neppplus.orderapp_20220618.adapters.StoreAdapter
 import com.neppplus.orderapp_20220618.datas.StoreData
+import kotlinx.android.synthetic.main.fragment_pizza_store.*
 
 class PizzaStoreFragment : Fragment() {
 
     val mPizzaStoreList = ArrayList<StoreData>()
+
+    lateinit var mAdapter: StoreAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +31,9 @@ class PizzaStoreFragment : Fragment() {
         mPizzaStoreList.add(  StoreData("파파존스", "1577-8080", 3.0, "http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800")  )
         mPizzaStoreList.add(  StoreData("도미노피자", "1577-3082", 5.0, "https://mblogthumb-phinf.pstatic.net/20160530_173/ppanppane_14646176567743d7hd_PNG/%B5%B5%B9%CC%B3%EB%C7%C7%C0%DA_%B7%CE%B0%ED_%282%29.png?type=w800")  )
         mPizzaStoreList.add(  StoreData("미스터피자", "1577-0077", 4.5, "https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200")  )
+
+        mAdapter = StoreAdapter( requireContext(), R.layout.store_list_item, mPizzaStoreList )
+        pizzaStoreListView.adapter = mAdapter
 
     }
 
